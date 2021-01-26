9360dab7fa25b042121d21e12d166b4cd3a1435b
yangsalt <kibkibe@naver.com>
2021-01-26 23:57:12 +0900

    update

--- ruby_character/ruby_character.js
+++ ruby_character/ruby_character.js
@@ -8,16 +8,24 @@ if (msg.type == "api"){
 			
 			let str = msg.content.replace("!루 ",'').replace("!r ",'');
 			const ruby_array = str.match(/\[[^\(\)\[\]]*\]\([^\(\)\[\]]+\)/g);
-			ruby_array.forEach(element => {
-				const split = element.split('](');
-				const rb = split[0].replace('[','');
-				const rt = split[1].replace(')','');
-				str = str.replace(element, '<div style="display:inline-block;vertical-align:bottom;">\
-				<div style="display:table"><div style="display:table-row;text-align:center;font-size:7pt;font-weight:bold;">'
-				+ rt + '</div><div style="display:table-row;text-align:center;">' + rb + '</div></div></div>');
-			});
+			if (ruby_array) {
+				ruby_array.forEach(element => {
+					const split = element.split('](');
+					const rb = split[0].replace('[','');
+					const rt = split[1].replace(')','');
+					str = str.replace(element, '<div style="display:inline-block;vertical-align:bottom;">\
+					<div style="display:table"><div style="display:table-row;text-align:center;font-size:7pt;font-weight:bold;">'
+					+ rt + '</div><div style="display:table-row;text-align:center;">' + rb + '</div></div></div>');
+				});
+			} else {
+				sendChat('error','/w "' + (msg.who==''?'GM':msg.who) + '" 루비 형식이 올바르지 않습니다.<br>``[아래에 표시될 문장]````(위에 표시될 문장)``의 형식으로 입력해주세요.',null,{noarchive:true});
+				return;
+			}
 
 			const findCharacterWithName = function(who) {
+			    if (who == "") {
+			        return '';
+			    }
 				let chat_cha = findObjs({ _type: 'character', name: who});
 				if (chat_cha.length > 0) {
 					return "character|" + chat_cha[0].get('_id');
