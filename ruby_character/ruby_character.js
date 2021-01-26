4523baea0d28454025aa22376c716755afc439f6
yangsalt <kibkibe@naver.com>
2021-01-26 21:46:59 +0900

    ruby_character

---
+++ ruby_character/ruby_character.js
@@ -0,0 +1,40 @@
+/* https://github.com/kibkibe/roll20-api-scripts/tree/master/ruby_character */
+/* (ruby_character.js) 210126 코드 시작 */
+on("chat:message", function(msg)
+{
+if (msg.type == "api"){
+	try {
+		if (msg.content.indexOf("!루 ") == 0 || msg.content.indexOf("!r ") == 0) {
+			
+			let str = msg.content.replace("!루 ",'').replace("!r ",'');
+			const ruby_array = str.match(/\[[^\(\)\[\]]*\]\([^\(\)\[\]]+\)/g);
+			ruby_array.forEach(element => {
+				const split = element.split('](');
+				const rb = split[0].replace('[','');
+				const rt = split[1].replace(')','');
+				str = str.replace(element, '<div style="display:inline-block;margin-bottom:-4px;">\
+				<div style="display:table"><div style="display:table-row;text-align:center;font-size:7pt;font-weight:bold;">'
+				+ rt + '</div><div style="display:table-row;text-align:center;">' + rb + '</div></div></div>');
+			});
+
+			const findCharacterWithName = function(who) {
+				let chat_cha = findObjs({ _type: 'character', name: who});
+				if (chat_cha.length > 0) {
+					return "character|" + chat_cha[0].get('_id');
+				} else {
+				    let chat_pl = findObjs({ _type: 'player', name: who});
+    				if (chat_pl.length > 0) {
+    					return "player|" + chat_pl[0].get('_id');
+    				} else {
+					    return who;
+    				}
+				}
+			}
+
+			sendChat(str.includes('/desc')?'':findCharacterWithName(msg.who),str);
+		}
+	} catch (err) {
+		sendChat('error','/w GM '+err,null,{noarchive:true});
+	}
+}});
+/* (ruby_character.js) 210126 코드 종료 */
